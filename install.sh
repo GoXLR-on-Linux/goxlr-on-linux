a9e24d88a65d03f330e835b0ab28125df7e320f4
lm41 <59062169+lm41@users.noreply.github.com>
2021-04-14 10:35:41 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,32 +8,42 @@ sudo rm -rf /etc/goxlr
 
 sudo mkdir /etc/goxlr
 cd /etc/goxlr
-sudo git clone https://github.com/lm41/goxlr-on-linux.git
+sudo git clone https://github.com/T-Grave/goxlr-on-linux.git
 cd goxlr-on-linux
 
 if [[ ! -z $APT_GET_CMD ]]; then
-    cd $HOME
     sudo apt-get install jackd2 pulseaudio-module-jack
     if [[ "$1" = "MINI" ]]; then
-        sudo echo "source /etc/goxlr/goxlr-on-linux/configure_goxlr_mini.sh" >> ".profile"
+        sudo chmod u+x /etc/goxlr/goxlr-on-linux/configure_goxlr_mini.sh
+        sudo systemctl enable run-at-startup.service
+        cd /etc/goxlr/goxlr-on-linux
+        sudo cp run-at-startup.service /etc/systemd/system/
     else
-        sudo echo "source /etc/goxlr/goxlr-on-linux/configure_goxlr.sh" >> ".profile"
+        sudo chmod u+x /etc/goxlr/goxlr-on-linux/configure_goxlr.sh
+        sudo systemctl enable run-at-startup.service
+        cd /etc/goxlr/goxlr-on-linux
+        sudo cp run-at-startup.service /etc/systemd/system/
     fi
 
 elif [[ ! -z $PACMAN_CMD ]]; then
     sudo pacman -S jack2 jack2-dbus pulseaudio-jack
-    cd $HOME
     if [[ "$1" = "MINI" ]]; then
-        sudo echo "source /etc/goxlr/goxlr-on-linux/configure_goxlr_mini.sh" | sudo tee -a  ".bash_profile" > /dev/null
+        sudo chmod u+x /etc/goxlr/goxlr-on-linux/configure_goxlr_mini.sh
+        sudo systemctl enable run-at-startup.service
+        cd /etc/goxlr/goxlr-on-linux
+        sudo cp run-at-startup.service /etc/systemd/system/
     else
-        sudo echo "source /etc/goxlr/goxlr-on-linux/configure_goxlr.sh" | sudo tee -a  ".bash_profile" > /dev/null
+        sudo chmod u+x /etc/goxlr/goxlr-on-linux/configure_goxlr.sh
+        sudo systemctl enable run-at-startup.service
+        cd /etc/goxlr/goxlr-on-linux
+        sudo cp run-at-startup.service /etc/systemd/system/
     fi
-    cd /etc/goxlr/goxlr-on-linux
     sudo cp audio.conf /etc/security/limits.d
 
 else
     echo "error can't install packages"
     exit 1;
 fi
+
 
 sudo reboot
