1a47591cb6c25d64dba293b43e1ff7cee536a115
lm41 <59062169+lm41@users.noreply.github.com>
2021-04-17 14:39:21 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,19 +11,19 @@ cd /etc/goxlr
 sudo git clone https://github.com/T-Grave/goxlr-on-linux.git
 cd goxlr-on-linux
 
-if [[ ! -z $APT_GET_CMD ]]; then
+if [ ! -z $APT_GET_CMD ]; then
     cd $HOME
     sudo apt-get install jackd2 pulseaudio-module-jack
-    if [[ "$1" = "MINI" ]]; then
+    if [ "$1" = "MINI" ]; then
         sudo echo "source /etc/goxlr/goxlr-on-linux/configure_goxlr_mini.sh" >> ".profile"
     else
         sudo echo "source /etc/goxlr/goxlr-on-linux/configure_goxlr.sh" >> ".profile"
     fi
 
-elif [[ ! -z $PACMAN_CMD ]]; then
+elif [ ! -z $PACMAN_CMD ]; then
     sudo pacman -S jack2 jack2-dbus pulseaudio-jack
     cd $HOME
-    if [[ "$1" = "MINI" ]]; then
+    if [ "$1" = "MINI" ]; then
         sudo echo "source /etc/goxlr/goxlr-on-linux/configure_goxlr_mini.sh" | sudo tee -a  ".bash_profile" > /dev/null
     else
         sudo echo "source /etc/goxlr/goxlr-on-linux/configure_goxlr.sh" | sudo tee -a  ".bash_profile" > /dev/null
